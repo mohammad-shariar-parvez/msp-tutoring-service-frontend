458b8bc618fa4468a4523cbcba844385c41784e5
Piaowei <msptonmoy@gmail.com>
2023-10-20 19:40:33 +0600

    feat(profile):profile added

---
+++ src/app/(withlayout)/admin/questions/page.tsx
@@ -0,0 +1,206 @@
+'use client';
+import {
+  DeleteOutlined,
+  EditOutlined,
+  ReloadOutlined,
+  EyeOutlined,
+} from '@ant-design/icons';
+import UMBreadCrumb from '@/components/ui/UMBreadCrumb';
+import UMTable from '@/components/ui/UMTable';
+
+import { Button, Input, Modal, message } from 'antd';
+
+import { useState } from 'react';
+import ActionBar from '@/components/ui/ActionBar';
+import { useDebounced } from '@/redux/hooks';
+import dayjs from 'dayjs';
+import {
+  useDeleteQuestionsMutation,
+  useFeeedbacksQuery,
+  useQuestionsQuery,
+} from '@/redux/api/feedback';
+
+const QuestionPage = () => {
+  const query: Record<string, any> = {};
+
+  const [page, setPage] = useState<number>(1);
+  const [size, setSize] = useState<number>(10);
+  const [sortBy, setSortBy] = useState<string>('');
+  const [sortOrder, setSortOrder] = useState<string>('');
+  const [searchTerm, setSearchTerm] = useState<string>('');
+  const [isModalOpen, setIsModalOpen] = useState(false);
+  const [details, setDetails] = useState<any>({});
+  const [deleteQuestions] = useDeleteQuestionsMutation();
+
+  const showModal = () => {
+    setIsModalOpen(true);
+  };
+
+  const handleCancel = () => {
+    setIsModalOpen(false);
+  };
+
+  query['limit'] = size;
+  query['page'] = page;
+  query['sortBy'] = sortBy;
+  query['sortOrder'] = sortOrder;
+  // query["searchTerm"] = searchTerm;
+
+  const debouncedTerm = useDebounced({
+    searchQuery: searchTerm,
+    delay: 600,
+  });
+
+  if (!!debouncedTerm) {
+    query['searchTerm'] = debouncedTerm;
+  }
+  const { data, isLoading } = useQuestionsQuery({ ...query });
+
+  const questions = data?.question;
+  const meta = data?.meta;
+  console.log(data);
+
+  const deleteHandler = async (id: string) => {
+    message.loading('Deleting.....');
+    try {
+      //   console.log(data);
+      const res = await deleteQuestions(id);
+      if (res) {
+        message.success('Blogs Deleted successfully');
+      }
+    } catch (err: any) {
+      //   console.error(err.message);
+      message.error(err.message);
+    }
+  };
+
+  const columns = [
+    {
+      title: 'Name',
+      dataIndex: 'name',
+    },
+    {
+      title: 'Location',
+      dataIndex: 'location',
+    },
+    {
+      title: 'Phone',
+      dataIndex: 'phone',
+    },
+    {
+      title: 'Requirement',
+      dataIndex: 'requirement',
+      render: function (data: any) {
+        return data.substring(0, 15).concat('.....');
+      },
+    },
+    {
+      title: 'CreatedAt',
+      dataIndex: 'createdAt',
+      render: function (data: any) {
+        return data && dayjs(data).format('MMM D, YYYY hh:mm A');
+      },
+      sorter: true,
+    },
+    {
+      title: 'Action',
+      render: function (data: any) {
+        return (
+          <>
+            <Button
+              onClick={() => onDetailsHandler(data)}
+              type='primary'
+              className='mr-2'
+            >
+              <EyeOutlined />
+            </Button>
+            <Button
+              onClick={() => deleteHandler(data?.id)}
+              type='primary'
+              danger
+            >
+              <DeleteOutlined />
+            </Button>
+          </>
+        );
+      },
+    },
+  ];
+  const onDetailsHandler = (values: any) => {
+    // console.log('Values', values);
+
+    setIsModalOpen(true);
+    setDetails(values);
+    // console.log(values);
+  };
+  const onPaginationChange = (page: number, pageSize: number) => {
+    // console.log('Page:', page, 'PageSize:', pageSize);
+    setPage(page);
+    setSize(pageSize);
+  };
+  const onTableChange = (pagination: any, filter: any, sorter: any) => {
+    const { order, field } = sorter;
+    // console.log(order, field);
+    setSortBy(field as string);
+    setSortOrder(order === 'ascend' ? 'asc' : 'desc');
+  };
+
+  const resetFilters = () => {
+    setSortBy('');
+    setSortOrder('');
+    setSearchTerm('');
+  };
+
+  return (
+    <div>
+      <UMBreadCrumb
+        items={[
+          {
+            label: 'admin',
+            link: '/admin',
+          },
+        ]}
+      />
+
+      <ActionBar title='Question List'>
+        <div className='ml-auto pr-4'>
+          {(!!sortBy || !!sortOrder || !!searchTerm) && (
+            <Button
+              onClick={resetFilters}
+              type='primary'
+              style={{ margin: '0px 5px' }}
+            >
+              <ReloadOutlined />
+            </Button>
+          )}
+        </div>
+      </ActionBar>
+
+      <UMTable
+        loading={isLoading}
+        columns={columns}
+        dataSource={questions}
+        pageSize={size}
+        totalPages={meta?.total}
+        showSizeChanger={true}
+        onPaginationChange={onPaginationChange}
+        onTableChange={onTableChange}
+        showPagination={true}
+      />
+      <>
+        <Modal
+          title='Requirements'
+          open={isModalOpen}
+          onCancel={handleCancel}
+          footer={null}
+        >
+          <div className=''>
+            <h1 className='font-normal text-base'>{details?.requirement}</h1>
+          </div>
+        </Modal>
+      </>
+    </div>
+  );
+};
+
+export default QuestionPage;
