c46d8604f294096f7293ef20fe2744ce4112793c
Piaowei <msptonmoy@gmail.com>
2023-10-28 02:09:43 +0600

    feat(course): course card filtering

--- src/app/(protected)/categories/[id]/page.tsx
+++ src/app/(protected)/categories/[id]/page.tsx
@@ -1,8 +1,9 @@
 'use client';
 import React, { useState } from 'react';
 import type { RadioChangeEvent } from 'antd';
-import { Button, Input, Radio } from 'antd';
+import { Button, Empty, Input, Pagination, Radio } from 'antd';
 import CourseCard from '@/components/ui/CourseCard';
+import type { PaginationProps } from 'antd';
 import {
   useServicesByCategoryQuery,
   useServicesQuery,
@@ -13,52 +14,113 @@ import {
   SearchOutlined,
   ArrowRightOutlined,
   ReloadOutlined,
+  CloseOutlined,
 } from '@ant-design/icons';
 import Form from '@/components/Forms/Form';
 import FormInput from '@/components/Forms/FormInput';
+import { useDebounced } from '@/redux/hooks';
 type IDProps = {
   params: any;
 };
 const Services = ({ params }: IDProps) => {
   const { id } = params;
-  const initailQuery: Record<string, any> = { serviceId: id };
+  const query: Record<string, any> = {};
   const [value, setValue] = useState('');
   const [formReset, setFormReset] = useState<boolean>(false);
-  const [query, setQuery] = useState({ ...initailQuery });
-  const [maxPrice, setMaxPrice] = useState<string>('');
-  const [minPrice, setMinPrice] = useState<string>('');
+  const [page, setPage] = useState<number>(1);
+  const [size, setSize] = useState<number>(12);
+  const [sortBy, setSortBy] = useState<string>('');
+  const [sortOrder, setSortOrder] = useState<string>('');
+  const [searchTerm, setSearchTerm] = useState<string>('');
+  const [maxPrice, setMaxPrice] = useState<string | undefined>(undefined);
+  const [minPrice, setMinPrice] = useState<string | undefined>(undefined);
+  const [location, setLocation] = useState<string | undefined>(undefined);
+  const [serviceId, setServiceId] = useState(id);
+  query['location'] = location;
+  query['minPrice'] = minPrice;
+  query['maxPrice'] = maxPrice;
+  query['serviceId'] = serviceId;
+  query['limit'] = size;
+  query['page'] = page;
+  query['sortBy'] = sortBy;
+  query['sortOrder'] = sortOrder;
+
+  //PAGINATION
+  const onPageChange: PaginationProps['onChange'] = (page, pageSize) => {
+    console.log(pageSize);
+    console.log(page);
+    // setSize(pageSize);
+
+    setPage(page);
+  };
+
+  // SEARCHING
+  const debouncedTerm = useDebounced({
+    searchQuery: searchTerm,
+    delay: 600,
+  });
+
+  if (!!debouncedTerm) {
+    query['searchTerm'] = debouncedTerm;
+  }
+
+  const resetSearchFilters = async () => {
+    setSearchTerm('');
+  };
 
   const onChange = (e: RadioChangeEvent) => {
-    setQuery({ ...query, ['location']: e.target.value });
+    setLocation(e.target.value);
     setValue(e.target.value);
   };
-  const { data } = useServicesQuery(query);
-  const coursesData: IService[] = (data?.services || []) as IService[];
-
-  console.log('HELLLOOOO', coursesData);
-
   const publicOnSubmit = async (values: any) => {
-    console.log(values);
-    setQuery({ ...query, ...values });
     setMinPrice(values.minPrice);
     setMaxPrice(values.maxPrice);
   };
 
   const resetFilters = async () => {
     setFormReset(true);
-    setMaxPrice('');
-    setMinPrice('');
+    setLocation(undefined);
+    setMaxPrice(undefined);
+    setMinPrice(undefined);
+    setServiceId(id);
     setValue('');
-    setQuery({ serviceId: id });
+    setSearchTerm('');
   };
+
+  const { data } = useServicesQuery({ ...query });
+  const coursesData: IService[] = (data?.services || []) as IService[];
+  console.log('HELLLOOOO', coursesData);
+  console.log('META', data?.meta);
+
   return (
     <div className='container  '>
       <div className='grid grid-cols-4 gap-6'>
-        <div className=''>
+        <div className='space-y-4'>
+          <div className='relative'>
+            <Input
+              type='text'
+              size='large'
+              placeholder='Search...'
+              value={searchTerm}
+              onChange={(e) => {
+                setSearchTerm(e.target.value);
+              }}
+              className='pr-8'
+            />
+            {!!searchTerm && (
+              <Button
+                onClick={resetSearchFilters}
+                type='primary'
+                className='m-0 p-0 bg-transparent shadow-none text-gray-600 absolute right-2 bottom-0'
+              >
+                <CloseOutlined className='text-lg bg-transparent' />
+              </Button>
+            )}
+          </div>
           <Radio.Group
             onChange={onChange}
             value={value}
-            className=' flex flex-col gap-4'
+            className=' flex flex-col gap-2'
           >
             {locationOptions.map((location, index) => (
               <Radio key={index} value={location.value}>
@@ -67,7 +129,7 @@ const Services = ({ params }: IDProps) => {
             ))}
           </Radio.Group>
           <Form submitHandler={publicOnSubmit} noReset={formReset}>
-            <div className='flex justify-between items-center w-full space-x-1 py-4'>
+            <div className='flex justify-between items-center w-full space-x-1 pb-2'>
               <FormInput
                 name='minPrice'
                 size='small'
@@ -75,19 +137,16 @@ const Services = ({ params }: IDProps) => {
                 placeholder='Min Price'
               />
 
-              {/* <!-- Address Field  --> */}
-
               <FormInput
                 name='maxPrice'
                 size='small'
                 type='number'
                 placeholder='Max Price'
               />
-              {/* <!-- Find Button  --> */}
 
               <Button
                 htmlType='submit'
-                className=' bg-transparent  font-bold text-md p-0 m-0 rounded-md  cursor-pointer transition duration-700 border-0 '
+                className=' bg-transparent  font-bold text-md p-0 m-0 rounded-md  cursor-pointer transition duration-700 border-0 m '
               >
                 <SearchOutlined />
               </Button>
@@ -98,10 +157,25 @@ const Services = ({ params }: IDProps) => {
           </Form>
         </div>
         <div className='col-span-3'>
-          <div className='grid grid-cols-2 md:grid-cols-4 gap-4'>
-            {coursesData?.map((course: IService) => (
-              <CourseCard key={course.id} course={course} />
-            ))}
+          {coursesData?.length > 0 ? (
+            <div className='grid grid-cols-2 md:grid-cols-4 gap-4 '>
+              {coursesData?.map((course: IService) => (
+                <CourseCard key={course.id} course={course} />
+              ))}
+            </div>
+          ) : (
+            <Empty description='No courses found' />
+          )}
+
+          <div className='flex justify-end pt-12 '>
+            <Pagination
+              defaultCurrent={page}
+              onChange={onPageChange}
+              defaultPageSize={size}
+              showSizeChanger={false}
+              total={data?.meta?.total}
+              showQuickJumper
+            />
           </div>
         </div>
       </div>
