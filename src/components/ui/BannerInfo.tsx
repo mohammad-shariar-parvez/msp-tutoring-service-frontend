7da31ee6e8e43df4d1d3b2ecf58f9990fb3bda68
Piaowei <msptonmoy@gmail.com>
2023-11-22 13:33:46 +0600

    update(statistics):react count up added

--- src/components/ui/BannerInfo.tsx
+++ src/components/ui/BannerInfo.tsx
@@ -1,6 +1,7 @@
 'use client';
 import { Col, Row } from 'antd';
 import React from 'react';
+import CountUp from 'react-countup';
 import {
   UsergroupDeleteOutlined,
   BookFilled,
@@ -9,6 +10,21 @@ import {
 } from '@ant-design/icons';
 
 const BannerInfo = () => {
+  const formatter = (value: number, title: string) => (
+    <CountUp className='statistics-details' end={value} separator=','>
+      {({ countUpRef, start }) => (
+        <div className='ml-[5px]'>
+          <span
+            className='text-[26px] font-bold text-[#333]'
+            ref={countUpRef}
+          />
+          <span className='text-[26px] font-bold text-[#333]'>+</span>
+          <p className='mb-0  text-base text-[#333] font-medium'>{title}</p>
+        </div>
+      )}
+    </CountUp>
+  );
+
   return (
     <section>
       <div className='bg-[#f8fbff] '>
@@ -17,39 +33,26 @@ const BannerInfo = () => {
             <Col xs={12} md={12} lg={6}>
               <div className='sg-bnr-info-itm'>
                 <UsergroupDeleteOutlined className=' icon-size clr-01' />
-                <div className='sg-bnr-info-details'>
-                  <h5>5000 +</h5>
-                  <p>Active Tutors</p>
-                </div>
+                {formatter(30, 'Active Tutors')}
               </div>
             </Col>
             <Col xs={12} md={12} lg={6}>
               <div className='sg-bnr-info-itm'>
                 <UserOutlined className=' icon-size clr-02' />
-                <div className='sg-bnr-info-details'>
-                  <h5>9000 +</h5>
-                  <p>Active Students</p>
-                </div>
+                {formatter(900, 'Active Students')}
               </div>
             </Col>
             <Col xs={12} md={12} lg={6}>
               <div className='sg-bnr-info-itm'>
                 <BookFilled className=' icon-size clr-03' />
-                <div className='sg-bnr-info-details'>
-                  <h5>40 +</h5>
-                  <p>+Subject</p>
-                </div>
+                {formatter(900, 'Subject')}
               </div>
             </Col>
             <Col xs={12} md={12} lg={6}>
               <div className='sg-bnr-info-itm'>
                 <i className='fas fa-map-marked-alt fa-fw clr-04'></i>
                 <CarryOutFilled className=' icon-size clr-04' />
-
-                <div className='sg-bnr-info-details'>
-                  <h5>2000 +</h5>
-                  <p>Locations Cover</p>
-                </div>
+                {formatter(20, 'Locations Cover')}
               </div>
             </Col>
           </Row>
