62397f1200415902776c20d47911e8955d1d9cb8
Piaowei <msptonmoy@gmail.com>
2023-11-25 09:01:39 +0600

    update(style):card style updated

--- src/app/(private)/course/[id]/page.tsx
+++ src/app/(private)/course/[id]/page.tsx
@@ -22,89 +22,76 @@ const ServiceCourse = ({ params }: IDProps) => {
   const da = getUserInfo() as any;
   const router = useRouter();
 
-  // useEffect(() => {
-  //   // console.log(userLoggedIn);
-  //   if (!userLoggedIn) {
-  //     router.push('/login');
-  //   }
-  // }, []);
-
-  // const { data: session, status } = useSession();
-  // console.log(session);
-
   const { data, isLoading } = useCourseQuery(id);
   // console.log(data);
 
   const courseData = data;
 
   return (
-    <div className='container  py-12'>
-      <div className='bg-white p-4 '>
-        <div>
-          <Row>
-            <Col xs={24} md={24} lg={4}>
+    <section className='bg-[#f3f6fd]  py-16'>
+      <div className='container  '>
+        <div className=' py-8 my-8 bg-white p-4 rounded-md '>
+          <div className='grid grid-cols-1 md:grid-cols-12 gap-4 '>
+            <div className='md:col-span-2'>
               <Image
                 src={data?.courseTutor?.imageUrl}
                 width={150}
                 height={150}
                 alt='eagle_image'
-                className='rounded-full block mx-auto mt-5 '
+                // className='rounded-full block mx-auto mt-5 '
               />
-            </Col>
+            </div>
 
-            <Col xs={24} md={24} lg={15}>
-              <div className='p-4'>
-                <h1 className='mb-3    text-2xl '>
-                  {courseData?.courseTutor?.firstName}{' '}
-                  {courseData?.courseTutor?.middleName}{' '}
-                  {courseData?.courseTutor?.lastName}
-                </h1>
+            <div className='md:col-span-8'>
+              <h1 className='mb-3   font-semibold text-2xl text-secondary '>
+                {courseData?.courseTutor?.firstName}{' '}
+                {courseData?.courseTutor?.middleName}{' '}
+                {courseData?.courseTutor?.lastName}
+              </h1>
 
-                <p className='text-gray-500 text-base'>
-                  {courseData?.courseTutor?.bio}
-                </p>
+              <p className='text-base font-normal text-[#212529]   m-auto space-y-4 text-justify'>
+                {courseData?.courseTutor?.bio}
+              </p>
 
-                <div className='py-2'>
+              {/* <div className='py-2'>
                   <Button
                     htmlType='submit'
                     className='hidden md:block bg-button-primary  text-white   px-3  rounded-md  '
                   >
                     Contact
                   </Button>
-                </div>
-              </div>
-            </Col>
+                </div> */}
+            </div>
 
-            <Col xs={24} md={24} lg={5} className='p-4'>
-              <div>
-                <Row justify='space-between' className='pb-4'>
+            <div className='md:col-span-2 '>
+              <div className=' space-y-4 text-base font-normal text-secondary   text-justify'>
+                <div className=' flex justify-between'>
                   <strong>Experience</strong>
                   <p>{courseData?.courseTutor?.experience}</p>
-                </Row>
-                <Row justify='space-between' className='pb-4'>
+                </div>
+                <div className=' flex justify-between items-center'>
                   <strong>Gender</strong>
-
                   <p>{courseData?.courseTutor?.gender}</p>
-                </Row>
+                </div>
               </div>
-            </Col>
-          </Row>
+            </div>
+          </div>
         </div>
-      </div>
-      {/* Line */}
+        {/* Line */}
 
-      <div className='flex justify-start bg-white p-4 my-5 divide-x'>
-        <div className='px-4 font-bold text-lg space-x-5 '>
-          <span>Information</span>
-          <span>Article</span>
+        <div className='flex justify-start   divide-x text-secondary my-16 px-4 bg-white rounded-md '>
+          <div className='  space-x-5 text-lg font-semibold py-4'>
+            <span>Article</span>
+          </div>
         </div>
-      </div>
-      {/*  INFO*/}
-      <div className=' my-5 divide-y '>
-        <Row gutter={[17, 17]}>
-          <Col xs={24} md={24} lg={14}>
-            <div className='bg-white p-4 space-y-4'>
-              <h1 className='mb-6  mt-3  text-3xl    '>Course Information</h1>
+
+        {/*  INFO*/}
+        <div className='  divide-y  grid grid-cols-1 md:grid-cols-12 gap-8 gap-y-20 pb-16  '>
+          <div className='c md:col-span-7 '>
+            <div className='bg-white   p-4 rounded-md'>
+              <h1 className='mb-6  mt-3  text-3xl text-secondary   font-medium '>
+                Course Information
+              </h1>
               <Image
                 src={courseData?.imageUrl}
                 width={250}
@@ -112,33 +99,29 @@ const ServiceCourse = ({ params }: IDProps) => {
                 alt='eagle_image'
                 className='  object-fill    '
               />
-              <hr />
-              <p className='text-gray-500 text-base'>
+
+              <p className='text-gray-500 text-base mt-4'>
                 {courseData?.description}
               </p>
             </div>
-            {/* Review */}
-            <RatingReview courseId={id} />
-          </Col>
-          {/* Stepper */}
-          <Col xs={24} md={24} lg={10}>
-            {!isLoading ? <BookingFormSection courseId={id} /> : null}
-          </Col>
-        </Row>
-        {/* Comments */}
-        <Row>
-          <Col xs={24} md={24} lg={24}>
-            <CommentsSection id={id} />
-          </Col>
-        </Row>
-      </div>
 
-      <RelatedCourse
-        categoryId={courseData?.categoryId}
-        location={courseData?.location}
-      />
-      {/* <Footer /> */}
-    </div>
+            <RatingReview courseId={id} />
+          </div>
+          {/* Stepper */}
+          <div className='  md:col-span-5 '>
+            {!isLoading ? <BookingFormSection courseId={id} /> : null}
+          </div>
+        </div>
+
+        <CommentsSection id={id} />
+
+        <RelatedCourse
+          categoryId={courseData?.categoryId}
+          location={courseData?.location}
+        />
+        {/* <Footer /> */}
+      </div>
+    </section>
   );
 };
 
